70ac61a997b4416312d1c66fb1c2f51ab3328a05
LosPollosHermanos <afatahi201580@gmail.com>
2024-02-16 14:58:57 +0330

    js: flow control part1

--- main.js
+++ main.js
@@ -12,3 +12,42 @@ const birthYear = 1995;
 const currentYear = 2024;
 const myAge = ((currentYear - birthYear) * 365) / 7 ;
 console.log(myAge + " weeks of my life was passed...");
+
+// #3 if else
+const role = "ADMIN";
+if(role === "ADMIN"){
+    console.log("the user role is admin");
+}
+else if(role === "MERCHANT"){
+    console.log("the user role is merchant");
+}
+else if(role === "TEACHER"){
+    console.log("the user role is teacher");
+}
+else{
+    console.log("unknown user role...");
+}
+
+// #4 for in
+const user = {
+    name : "Ali",
+    email : "afatahi201580@gmail.com",
+    phoneNumber : "09380464481"
+};
+for(const key in user){
+    console.log(key, ":", user[key]);
+}
+
+// #5 for of
+const roles = ["AMDIN", "TEACHER", "MERCHANT"];
+for(const item of roles){
+    console.log(item);
+}
+
+//  #6 function - circle area
+const PI = 3.14;
+function calculateArea(radius){
+    const area = PI * radius ** 2
+    console.log("The Circle area is equal to ", area);
+}
+calculateArea(10);
